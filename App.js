b0c3ed7a599012ecbcf71c18376ef16df4f7cb38
yrrab911 <barry.leidy@gmail.com>
2022-11-26 18:18:45 -0700

    second comment.

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState, } from 'react';
-//step 2 delete AsyncStorage
 import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput, Button, Alert } from 'react-native';
 import  Navigation from './components/Navigation';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -14,26 +13,21 @@ const App = () =>{
   const [phoneNumber, setPhoneNumber] = React.useState("");
   const [homeTodayScore, setHomeTodayScore] = React.useState(0);
   const [tempCode , setTempCode] = React.useState(null);
-  //useEffect does special effects before the app loads (the behind the scenes)
-  //step 3 (two const lines and getSessionToken, reload app and look for token from storage on the terminal)
+  
   useEffect(()=>{
-  //initializing the variable in order to store it
-  const getSessionToken = async() => {
-    const sessionToken = await AsyncStorage.getItem('sessionToken');
-    console.log('token from storage', sessionToken);
-    //step 4 add validateResponse to get the email adress
-    const validateResponse = await fetch('https://dev.stedi.me/validate/' + sessionToken);
-    //token is valid if it is 200
-    if(validateResponse.status == 200){
-      const userEmail = await validateResponse.text();
-      await AsyncStorage.setItem('userName',userEmail);
-      console.log('userEmail', userEmail);
-      setIsLoggedIn(true);
-    }
+    const getSessionToken = async() => {
+      const sessionToken = await AsyncStorage.getItem('sessionToken');
+      console.log('token from storage', sessionToken);
+      const validateResponse = await fetch('https://dev.stedi.me/validate/'+sessionToken);
+      if(validateResponse.status == 200){
+        const userEmail = await validateResponse.text();
+        await AsyncStorage.setItem('userEmail', userEmail);
+        console.log('userEmail', userEmail);
+        setIsLoggedIn(true);
+      }
     }
     getSessionToken();
   },[])
-  //if they haven't loggen in yet they are directed to OnboardingScreen
    if (isFirstLaunch == true &&! isLoggedIn){
 return(
   <OnboardingScreen setFirstLaunch={setFirstLaunch}/>
@@ -55,7 +49,7 @@ return(
           style={styles.button}
           onPress={async()=>{
             console.log('Button was pressed')
-            const sendTextResponce = await fetch(
+            const sendTextResponse=await fetch(
               'https://dev.stedi.me/twofactorlogin/'+ phoneNumber,
               {
                 method: 'POST',
@@ -64,9 +58,9 @@ return(
                 }
               }
             )
-            if (sendTextResponce.status!=200){
-              console.log('Server send text responce: '+sendTextResponce.status)
-              alert('communication error','Server responded to send text with status: '+sendTextResponce.status);
+            if (sendTextResponse.status!=200){
+              console.log('Server send text response: '+sendTextResponse.status);
+              Alert('Communication Error','Server responded to send text with status: '+sendTextResponse.status);
             }
           }}
         />
@@ -98,16 +92,14 @@ return(
               console.log(loginResponse.status)
               if(loginResponse.status == 200){
                 const sessionToken = await loginResponse.text();
-                await AsyncStorage.setItem('sessionToken', sessionToken)
+                await AsyncStorage.setItem('sessionToken', sessionToken);
                 console.log('session token', sessionToken);
-                //add this line to save token onto the hard drive of the phone
-                //we want this app to only make you log in once and then it will store the data and always log you in automatically
-                //step 1
                 setIsLoggedIn(true);
               }
               else{
-                console.log("token resonce Status",loginResponse.status)
+                console.log("token responce Status",loginResponse.status)
                 Alert.alert('Warning' , 'An invalid Code was entered')
+                setIsLoggedIn(false);
               }
           }}
         />
--- screens/Profile.js
+++ screens/Profile.js
@@ -4,47 +4,32 @@ import { Card, CardTitle, CardContent} from 'react-native-material-cards';
 import BarChart from 'react-native-bar-chart';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Camera } from 'expo-camera';
-
-
-const cameraOptions={
-quality:0,
-exif:false
-}
 // import Share from 'react-native-share';
 
+const cameraOptions={
+  quality:0,
+exif:false}
 
-
-// const data = [
-//   [70, 0],
-//   [80,0],
-//   [110, 0],
-//   [100, 0],
-//   [280, 0],
-//   [80,0 ],
-//   [110, 0]
-
-// ];
-// labels
-// const horizontalData = ['S', 'M', 'T', 'W', 'T', 'F','S'];
 
 const Profile = (props) => {
   const [userName,setUserName] = useState("");
-    const [cameraPermission, setCameraPermission] = useState(false);
-    const[profilePhoto, setProfilePhoto] = useState(null);
-    const cameraRef = useRef(null);
-    const [cameraReady,setCameraReady] = useState(false);
-    useEffect(()=>{
-      const getUserName = async ()=>{
-        const cameraPermission = await Camera.requestCameraPermissionsAsync();
-        setCameraPermission(cameraPermission);
-        const userName = await AsyncStorage.getItem('userName');
-        setUserName(userName);
-        await AsyncStorage.removeItem('profilePhoto');
-        const profilePhoto = await AsyncStorage.getItem('profilePhoto');
-        setProfilePhoto(profilePhoto);
-      };
-      getUserName();
-    },[]);
+  // const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [cameraPermission, setCameraPermission] = useState(false);
+  const [profilePhoto, setProfilePhoto] = useState(null);
+  const cameraRef = useRef(null);
+  const [cameraReady, setCameraReady] = useState(false);
+useEffect(()=>{
+  const getUserName = async ()=>{
+    const cameraPermission = await Camera.requestCameraPermissionsAsync();
+    console.log('Camera permission', cameraPermission);
+    setCameraPermission(cameraPermission);
+    const userName = await AsyncStorage.getItem('userEmail');
+    setUserName(userName);
+    const profilePhoto = await AsyncStorage.getItem('profilePhoto')
+    setProfilePhoto(profilePhoto);
+  };
+  getUserName();
+},[]);
 
   const myCustomerShare = async() =>{
     const shareOptions = {
@@ -58,25 +43,29 @@ const Profile = (props) => {
   console.log('Error', error)
       }
     }
-    if(profilePhoto==null){
-      return (
-        <View style={styles.container}>
-          <Camera style={styles.container} ref={cameraRef} onCameraReady={()=>{setCameraReady(true)}}>
-            <View style={styles.buttonContainer}>
-              {cameraReady?<TouchableOpacity style={styles.button} onPress={async ()=>{
-                const Picture = await cameraRef.current.takePictureAsync(cameraOptions);
-                console.log('Picture',Pitcture);
-                await AsyncStorage.setItem('profilePhoto',Picture.uri);
-                setProfilePhoto(Picture.uri);
-              }}>
-                <Text style={styles.text}>Take Picture</Text>
-              </TouchableOpacity>: null}
-            </View>
-          </Camera>
+  if (!cameraPermission) {
+    return <View/>;
+  }
+if (profilePhoto==null){
+  return (
+    <View style={styles.container}>
+      <Camera style={styles.camera} ref={cameraRef} onCameraReady={()=>{setCameraReady(true)}}>
+        <View style={styles.buttonContainer}>
+          {cameraReady?<TouchableOpacity style={styles.button} onPress={async()=> {
+
+            const picture = await cameraRef.current.takePictureAsync(cameraOptions)
+            console.log('Picture', picture);
+            await AsyncStorage.setItem('profilePhoto',picture.uri);
+            setProfilePhoto(picture.uri);
+          }}>
+            <Text style={styles.text}>take Picture</Text>
+          </TouchableOpacity>:null}
         </View>
-      )
-    }
-else{
+      </Camera>
+    </View>
+  );
+}
+else {
   return (
     <SafeAreaView style={{flex: 1}}>
          <Card style={{backgroundColor:'white', borderRadius: 10, margin:20 ,width: 320, shadowColor: "#000",
@@ -90,7 +79,7 @@ shadowRadius: 2.62,
 elevation: 4}}>
      <CardContent>
      <Image style={{height: 100, width:100, borderRadius: 75}}
-      source={{uri:profilePhoto}} />
+      source={require('../image/me.jpg')} />
     <Text style={{marginTop:10,marginBottom:10,fontWeight: 'bold'}}>{userName}</Text>
 
     <Text style={{marginTop:20,marginBottom:2}}>This Week's progress</Text>
